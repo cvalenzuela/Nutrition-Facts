f30a3504a4a2ca63b14b2e6b6e5c7b415ba1ebf3
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2016-10-12 23:03:42 -0400

    change for objects

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
+/*
+==========
+
+Nutrition Fact, a Chrome Extension: get to know the content of the websites you visit.
+ICM Assignment.
+October 2016.
+
+cvalenzuela@nyu.edu
+
+==========
+*/
+
 // p5 can not be executed the normal "global" way
 // Instead a sketch instance has to be manually created
 // This is done with the closure below
-
 console.log("reading sketch...");
-/* Inverse the all words in p tags */
-// var text = document.getElementsByTagName('p');
-//
-// for(var k = 0; k < text.length; k++){
-//   var txt = text[k].innerHTML;
-//   var words = txt.split(/\s+/);
-//   words = words.reverse();
-//   txt = words.join(' ');
-//   text[k].innerHTML = txt;
-// }
 
 /* Variables */
 var rectX, rectY, title, xAlign, ratio, servigSize,
@@ -25,6 +26,7 @@ var sketch = function(p5) {
   p5.setup = function() {
     var c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
     c.position(0,0);
+    //c.style('z-index:9999');
     p5.clear();
 
     /* Setup Variables */
@@ -38,227 +40,90 @@ var sketch = function(p5) {
 
     /* Rectangle */
     p5.fill(255);
-    p5.rect(rectX,rectY,220,370);
+    p5.stroke(0);
+    p5.strokeWeight(3);
+    p5.rect(rectX,rectY,225,370);
 
-    /* Title */
-    var title = p5.createElement('h1', 'Nutrition Facts');
-    title.addClass('nutritionFacts');
-    title.addClass('nutritionFacts-title');
-    title.position(xAlign,rectY-3);
+    /* Get Web Tags */
+    var div = document.getElementsByTagName("div");
+    var imgDiv = document.getElementsByTagName("img");
+    var ifrmaeDiv = document.getElementsByTagName("iframe");
+    var pDiv = document.getElementsByTagName("p");
+    var figureDiv = document.getElementsByTagName("figure");
+    var aDiv = document.getElementsByTagName("a");
+    var linkDiv = document.getElementsByTagName("link");
+    var scriptDiv = document.getElementsByTagName("script");
+    var h1Div = document.getElementsByTagName("h1");
+    var h2Div = document.getElementsByTagName("h2");
+    var h3Div = document.getElementsByTagName("h3");
+    var h4Div = document.getElementsByTagName("h4");
+    var h5Div = document.getElementsByTagName("h5");
+    var h6Div = document.getElementsByTagName("h6");
+    var htotal = h1Div.length + h2Div.length + h3Div.length + h4Div.length + h5Div.length + h6Div.length;
+    var totalDiv = imgDiv.length + ifrmaeDiv.length + pDiv.length + figureDiv.length + aDiv.length + linkDiv.length + scriptDiv.length + htotal;
+    console.log("Total Div: "+totalDiv);
+    console.log("Total h:" + htotal);
+    console.log(round(imgDiv.length, totalDiv));
+    /* Nutrition Fact Object */
+    function NutritionFact(type, name, x, y, classes){
+      this.x = xAlign + x;
+      this.y = rectY + y;
+      this.fact = p5.createElement(type, name);
+      this.fact.position(this.x, this.y);
+      this.fact.addClass('nutritionFact');
+      for(var i = 0; i < classes.length ; i++){
+        this.fact.addClass(classes[i]);
+      }
+    }
 
-    /* Serving Size */
-    var servigSize = p5.createElement('p', 'Serving Size ' + p5.windowWidth + '/' + p5.windowHeight + ' px ' + '(' + ratioDecimals + ' ratio)');
-    servigSize.addClass('nutritionFacts');
-    servigSize.addClass('nutritionFacts-light');
-    servigSize.position(xAlign,rectY+28);
+    function round(a,b){
+      if(a == 0 || b == 0){
+        var ratiod = 0;
+        return ratiod;
+      }
+      else{
+        var ratiod = Math.round((a/b)*100);
+        return ratiod;
+      }
+    }
 
-    /* Serving per Container */
-    var servingPerContainer = p5.createElement('p', 'Serving Per Container About 1');
-    servingPerContainer.addClass('nutritionFacts');
-    servingPerContainer.addClass('nutritionFacts-light');
-    servingPerContainer.addClass('nutritionFacts-underline');
-    servingPerContainer.addClass('nutritionFacts-underlineThick');
-    servingPerContainer.position(xAlign,rectY+42);
+    /* Instance of Nutrition Facts */
+    var title = new NutritionFact('h1', 'Nutrition Facts',0, -3, ['nutritionFacts-title']);
+    var servigSize = new NutritionFact('p', 'Serving Size ' + p5.windowWidth + '/' + p5.windowHeight + ' px ' + '(' + ratioDecimals + ' ratio)',0, 28, ['nutritionFacts-light']);
+    var servingPerContainer = new NutritionFact('p', 'Serving Per Container About 1',0, 42, ['nutritionFacts-light','nutritionFacts-underline','nutritionFacts-underlineThick']);
+    var amountPerServing = new NutritionFact('p', 'Amount Per Serving',0, 68,['nutritionFacts-bold','nutritionFacts-underline'] );
+    var calories = new NutritionFact('p', 'Div Calories', 0, 84,['nutritionFacts-bold','nutritionFacts-subTitle', 'nutritionFacts-underline','nutritionFacts-underlineSemiThick'  ] );
+    var caloriesNumber = new NutritionFact('p', div.length, 74, 86,['nutritionFacts-light'] );
+    var caloriesFromFat = new NutritionFact('p', 'Calories from Div 40', 118, 85, ['nutritionFacts-light'] );
+    var dailyValue =  new NutritionFact('p', '% Daily Value*', 140, 108, ['nutritionFacts-bold']);
+    var totalImages = new NutritionFact('p', 'Total Images', 0, 124, ['nutritionFacts-topline','nutritionFacts-bold','nutritionFacts-subTitle', 'borderbottom']);
+    var totalImagesNumber = new NutritionFact('p', imgDiv.length + ' imgs', 78, 127, ['nutritionFacts-light']);
+    var totalImagesPerc = new NutritionFact('p', round(imgDiv.length, totalDiv) + '%', 176, 126, ['nutritionFacts-bold','nutritionFacts-subTitle']);
+    var iframe  = new NutritionFact('p', 'iframe ' + ifrmaeDiv.length, 20, 143, ['nutritionFacts-light','nutritionFacts-underlineSubmenu','width190']);
+    var iframePerct  = new NutritionFact('p', round(ifrmaeDiv.length, totalDiv) + ' %', 176, 143, ['nutritionFacts-bold','nutritionFacts-subTitle']);
+    var figures  = new NutritionFact('p', 'Figures ' + figureDiv.length ,20,159, ['nutritionFacts-light','nutritionFacts-underlineSubmenu','width190']);
+    var paragraphs = new NutritionFact('p', 'Paragraphs',0,175,['nutritionFacts-bold','nutritionFacts-subTitle','nutritionFacts-underline']);
+    var paragraphsNumber = new NutritionFact('p', pDiv.length + ' p',69,177, ['nutritionFacts-light']);
+    var paragraphsPerc = new NutritionFact('p', round(pDiv.length, totalDiv) + '%', 176, 175, ['nutritionFacts-bold','nutritionFacts-subTitle']);
+    var headlines = new NutritionFact('p', 'Headlines',0,194,['nutritionFacts-bold','nutritionFacts-underline']);
+    var headlinesNumber = new NutritionFact('p', htotal + ' p', 55, 194, ['nutritionFacts-light']);
+    var headlinesPerc = new NutritionFact('p', round(htotal, totalDiv) + '%', 185, 194, ['nutritionFacts-bold']);
+    var links = new NutritionFact('p', 'Total Links',0,213,['nutritionFacts-bold','nutritionFacts-underline']);
+    var linksNumber = new NutritionFact('p', aDiv.length+linkDiv.length + ' links',55,213,['nutritionFacts-light']);
+    var linksPerc = new NutritionFact('p', round(aDiv.length, totalDiv) + '%',176,213,['nutritionFacts-bold']);
+    var outside  =  new NutritionFact('p', 'Outside Links ', 20, 230, ['nutritionFacts-light','nutritionFacts-underlineSubmenu','width190']);
+    var outsidePerct  =  new NutritionFact('p', linkDiv.length + ' %',176,230,['nutritionFacts-bold','nutritionFacts-subTitle']);
+    var inside  =  new NutritionFact('p', 'Inside Links ' + aDiv.length, 20, 245, ['nutritionFacts-light','nutritionFacts-underlineSubmenu','width190']);
+    var javascript = new NutritionFact('p', 'Javascript',0,264, ['nutritionFacts-bold','nutritionFacts-subTitle','nutritionFacts-underline','nutritionFacts-underlineThick']);
+    var javascriptNumber = new NutritionFact('p', scriptDiv.length + ' files',62, 266, ['nutritionFacts-light']);
+    var verbs  = new NutritionFact('p', 'Verbs ', 0, 292, ['nutritionFacts-light','nutritionFacts-underline']); // change
+    var verbsPerct  = new NutritionFact('p', '5 ' + '%', 188, 292, ['nutritionFacts-bold','nutritionFacts-subTitle']); // change
+    var nouns  = new NutritionFact('p', 'Nouns ', 0, 308, ['nutritionFacts-light','nutritionFacts-underline']); // change
+    var nounsPerct  = new NutritionFact('p', '5 ' + '%', 188, 308, ['nutritionFacts-bold','nutritionFacts-subTitle'] ); // change
+    var disclosure  =  new NutritionFact('p', '*Percent Daily Values are based on a 8 hour watching at a screen. Your daily value may be higier or lower depending on your internet needs.', 0, 328,['nutritionFacts-light','font19']); // change
 
-    /* Amount per Serving */
-    var amountPerServing = p5.createElement('p', 'Amount Per Serving');
-    amountPerServing.addClass('nutritionFacts');
-    amountPerServing.addClass('nutritionFacts-bold');
-    amountPerServing.addClass('nutritionFacts-underline');
-    amountPerServing.position(xAlign,rectY+68);
-
-    /* Calories */
-    var calories = p5.createElement('p', 'Calories');
-    var caloriesNumber = p5.createElement('p', '230');  // change
-    var caloriesFromFat = p5.createElement('p', 'Calories from Fat 40');  // change
-    calories.addClass('nutritionFacts');
-    calories.addClass('nutritionFacts-bold');
-    calories.addClass('nutritionFacts-');
-    calories.addClass('nutritionFacts-underline');
-    calories.addClass('nutritionFacts-underlineSemiThick');
-    calories.position(xAlign,rectY+84);
-    caloriesNumber.addClass('nutritionFacts');
-    caloriesNumber.addClass('nutritionFacts-light');
-    caloriesNumber.position(xAlign + 53,rectY+85);
-    caloriesFromFat.addClass('nutritionFacts');
-    caloriesFromFat.addClass('nutritionFacts-light');
-    caloriesFromFat.position(xAlign + 118,rectY+85);
-    /* Calories from Fat */
-
-    /* % Daily Value* */
-    var dailyValue = p5.createElement('p', '% Daily Value*');
-    dailyValue.addClass('nutritionFacts');
-    dailyValue.addClass('nutritionFacts-bold');
-    dailyValue.position(xAlign + 140,rectY+108);
-
-    /* Total Images */
-    var totalImages = p5.createElement('p', 'Total Images');
-    var totalImagesNumber = p5.createElement('p', '8' + ' imgs'); // change
-    var totalImagesPerc = p5.createElement('p', '12' + '%'); // change
-    totalImages.addClass('nutritionFacts');
-    totalImages.addClass('nutritionFacts-topline');
-    totalImages.addClass('nutritionFacts-bold');
-    totalImages.addClass('nutritionFacts-');
-    totalImages.style('border-bottom:1px solid black;padding-bottom:2px;');
-    totalImages.position(xAlign,rectY+124);
-    totalImagesNumber.addClass('nutritionFacts');
-    totalImagesNumber.addClass('nutritionFacts-light');
-    totalImagesNumber.position(xAlign + 78,rectY+126);
-    totalImagesPerc.addClass('nutritionFacts');
-    totalImagesPerc.addClass('nutritionFacts-bold');
-    totalImagesPerc.addClass('nutritionFacts-');
-    totalImagesPerc.position(xAlign + 185,rectY+126);
-
-    /* Iframe and Avatar */
-    var iframe  = p5.createElement('p', 'iframe ' + '1'); // change
-    var iframePerct  = p5.createElement('p', '5 ' + '%'); // change
-    var avatars  = p5.createElement('p', 'Avatars ' + '1'); // change
-    iframe.addClass('nutritionFacts');
-    iframe.addClass('nutritionFacts-light');
-    iframe.position(xAlign + 20,rectY+143);
-    iframe.addClass('nutritionFacts-underlineSubmenu');
-    iframe.style('width: 190px;');
-    iframePerct.addClass('nutritionFacts');
-    iframePerct.addClass('nutritionFacts-bold');
-    iframePerct.addClass('nutritionFacts-');
-    iframePerct.position(xAlign + 188,rectY+143);
-    avatars.addClass('nutritionFacts');
-    avatars.addClass('nutritionFacts-light');
-    avatars.position(xAlign + 20,rectY+159);
-    avatars.addClass('nutritionFacts-underlineSubmenu');
-    avatars.style('width: 190px;');
-
-    /* Paragraphs */
-    var paragraphs = p5.createElement('p', 'Paragraphs');
-    var paragraphsNumber = p5.createElement('p', '12' + ' p'); // change
-    var paragraphsPerc = p5.createElement('p', '12' + '%'); // change
-    paragraphs.addClass('nutritionFacts');
-    paragraphs.addClass('nutritionFacts-bold');
-    paragraphs.addClass('nutritionFacts-');
-    paragraphs.position(xAlign,rectY+175);
-    paragraphs.addClass('nutritionFacts-underline');
-    paragraphsNumber.addClass('nutritionFacts');
-    paragraphsNumber.addClass('nutritionFacts-light');
-    paragraphsNumber.position(xAlign + 69,rectY+175);
-    paragraphsPerc.addClass('nutritionFacts');
-    paragraphsPerc.addClass('nutritionFacts-bold');
-    paragraphsPerc.addClass('nutritionFacts-');
-    paragraphsPerc.position(xAlign + 185,rectY+175);
-
-
-    /* Headlines */
-    var headlines = p5.createElement('p', 'Headlines');
-    var headlinesNumber = p5.createElement('p', '12' + ' p'); // change
-    var headlinesPerc = p5.createElement('p', '12' + '%'); // change
-    headlines.addClass('nutritionFacts');
-    headlines.addClass('nutritionFacts-bold');
-    headlines.addClass('nutritionFacts-');
-    headlines.position(xAlign,rectY+194);
-    headlines.addClass('nutritionFacts-underline');
-    headlinesNumber.addClass('nutritionFacts');
-    headlinesNumber.addClass('nutritionFacts-light');
-    headlinesNumber.position(xAlign + 58,rectY+194);
-    headlinesPerc.addClass('nutritionFacts');
-    headlinesPerc.addClass('nutritionFacts-bold');
-    headlinesPerc.addClass('nutritionFacts-');
-    headlinesPerc.position(xAlign + 185,rectY+194);
-
-    /* Total Links */
-    var links = p5.createElement('p', 'Total Links');
-    var linksNumber = p5.createElement('p', '4' + ' links'); // change
-    var linksPerc = p5.createElement('p', '22' + '%'); // change
-    links.addClass('nutritionFacts');
-    links.addClass('nutritionFacts-bold');
-    links.addClass('nutritionFacts-');
-    links.addClass('nutritionFacts-underline');
-    links.position(xAlign,rectY+213);
-    linksNumber.addClass('nutritionFacts');
-    linksNumber.addClass('nutritionFacts-light');
-    linksNumber.position(xAlign + 65,rectY+213);
-    linksPerc.addClass('nutritionFacts');
-    linksPerc.addClass('nutritionFacts-bold');
-    linksPerc.addClass('nutritionFacts-');
-    linksPerc.position(xAlign + 185,rectY+213);
-
-    /* Outside and Inside */
-    var outside  = p5.createElement('p', 'Outside Links '); // change
-    var outsidePerct  = p5.createElement('p', '5 ' + '%'); // change
-    var inside  = p5.createElement('p', 'Inside Links ' + '1'); // change
-    outside.addClass('nutritionFacts');
-    outside.addClass('nutritionFacts-light');
-    outside.position(xAlign + 20,rectY+230);
-    outside.addClass('nutritionFacts-underlineSubmenu');
-    outside.style('width: 190px;');
-    outsidePerct.addClass('nutritionFacts');
-    outsidePerct.addClass('nutritionFacts-bold');
-    outsidePerct.addClass('nutritionFacts-');
-    outsidePerct.position(xAlign + 188,rectY+230);
-    inside.addClass('nutritionFacts');
-    inside.addClass('nutritionFacts-light');
-    inside.position(xAlign + 20,rectY+245);
-    inside.addClass('nutritionFacts-underlineSubmenu');
-    inside.style('width: 190px;');
-
-    /* Javascript */
-    var javascript = p5.createElement('p', 'Javascript');
-    var javascriptNumber = p5.createElement('p', '12' + ' files'); // change
-    javascript.addClass('nutritionFacts');
-    javascript.addClass('nutritionFacts-bold');
-    javascript.addClass('nutritionFacts-');
-    javascript.position(xAlign,rectY+264);
-    javascript.addClass('nutritionFacts-underline');
-    javascript.addClass('nutritionFacts-underlineThick');
-    javascriptNumber.addClass('nutritionFacts');
-    javascriptNumber.addClass('nutritionFacts-light');
-    javascriptNumber.position(xAlign + 62,rectY+264);
-
-    /* Verbs */
-    var verbs  = p5.createElement('p', 'Verbs '); // change
-    var verbsPerct  = p5.createElement('p', '5 ' + '%'); // change
-    verbs.addClass('nutritionFacts');
-    verbs.addClass('nutritionFacts-light');
-    verbs.position(xAlign,rectY+292);
-    verbs.addClass('nutritionFacts-underline');
-    verbsPerct.addClass('nutritionFacts');
-    verbsPerct.addClass('nutritionFacts-bold');
-    verbsPerct.addClass('nutritionFacts-');
-    verbsPerct.position(xAlign + 188,rectY+292);
-
-    /* Nouns */
-    var nouns  = p5.createElement('p', 'Nouns '); // change
-    var nounsPerct  = p5.createElement('p', '5 ' + '%'); // change
-    nouns.addClass('nutritionFacts');
-    nouns.addClass('nutritionFacts-light');
-    nouns.position(xAlign,rectY+308);
-    nouns.addClass('nutritionFacts-underline');
-    nounsPerct.addClass('nutritionFacts');
-    nounsPerct.addClass('nutritionFacts-bold');
-    nounsPerct.addClass('nutritionFacts-');
-    nounsPerct.position(xAlign + 188,rectY+308);
-
-
-    /* Disclosure */
-    var disclosure  = p5.createElement('p', '*Percent Daily Values are based on a 8 hour watching at a screen. Your daily value may be higier or lower depending on your internet needs.'); // change
-    disclosure.addClass('nutritionFacts');
-    disclosure.addClass('nutritionFacts-light');
-    disclosure.style('font-size:9px;');
-    disclosure.position(xAlign,rectY+328);
 
   }
-
-
-  /* Draw lines if the user drags the mouse */
-  // p5.mouseDragged = function() {
-  //   p5.stroke(0);
-  //   p5.strokeWeight(4);
-  //   p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-  // }
-
-
-
-
-
-
 }
 
 /* The above function closure is passed into a p5 object constructor. this starts the sketch. */
