42ceadc36cd4a9feb425afdd442f6c1462963d17
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2016-10-12 23:07:44 -0400

    change for objects

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+/* Script that loads the app when clicked */
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "p5.min.js"}, function(){
       chrome.tabs.executeScript(null, {file: "p5.dom.js"}, function(){
